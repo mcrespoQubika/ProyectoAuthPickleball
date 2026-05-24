36d51e97433035a3757df44409124dc971bb82cd
Manuel.Crespo <manuel.crespo@192.168.1.3>
2026-05-24 19:33:18 -0300

    The const env is modified so that if ENVIRONMENT is undefined, STAGE values is used

--- src/helper/loginHelper.js
+++ src/helper/loginHelper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export function chooseEnvironment() {
-  const env = process.env.ENVIRONMENT.toUpperCase();
+  const env = (process.env.ENVIRONMENT ?? 'STAGE').toUpperCase();
   if (env === 'QA') return process.env.WEB_LOGIN_URL_QA;
   else if (env === 'TRAINING') return process.env.WEB_LOGIN_URL_TRAINING;
   else return process.env.WEB_LOGIN_URL_STAGE;
